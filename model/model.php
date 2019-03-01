7c21497c4e061e9e819c04d4a3f9504e82af2ddc
Julio <julio.espadas@gmail.com>
2019-03-01 14:27:55 +0100

    modification connect bdd

--- model/model.php
+++ model/model.php
@@ -1,13 +1,22 @@
 <?php
 
-function getUser($user)
-{
+function connectBD(){
+    $server = "mysql:host=localhost;dbname=room_reservation";
+    $username = "root";
+    $password = "root";
+    $options = null;
     try {
-        $go_bdd = new PDO("mysql:host=localhost;dbname=room_reservation", "root", "root", null);
+        $go_bdd = new PDO($server, $username, $password, $options);
     } catch (Exception $e) {
         die("Error: " . $e->getMessage());
     }
+}
 
+function getUser($user)
+{
+
+    connectBD();
+    global $go_bdd;
     $response = $go_bdd->query('SELECT * FROM users WHERE mail = "' . $user . '"');
     $userData = $response->fetch();
 
@@ -20,15 +29,11 @@ function getUser($user)
 
 function getRooms()
 {
-    try {
-        $go_bdd = new PDO("mysql:host=localhost;dbname=room_reservation", "root", "root", null);
-    } catch (Exception $e) {
-        die("Error: " . $e->getMessage());
-    }
-
+    connectBD();
+    global $go_bdd;
     $response = $go_bdd->query('SELECT DISTINCT * FROM rooms');
     $roomsData = $response->fetch();
-    $row = $roomsData;
+    //$row = $roomsData;
 
     if($roomsData) {
         return $roomsData;
